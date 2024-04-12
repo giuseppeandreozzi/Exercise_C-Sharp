340f9e5f782370aab4d4cb95814e98cfeb967411
giuseppeandreozzi <g.andreozzi98@gmail.com>
2024-04-12 13:03:27 +0200

    Added The Fountain of Objects exercise

---
+++ Level31_FountainOfObjects/Grid.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+
+public class Grid {
+    public (int width, int heigth) sizeGrid { get; set; } = (4, 4);
+    public (int x, int y) positionFountain { get; set; } = (0, 2);
+    public (int x, int y) positionPlayer { get; set; } = (0, 0);
+    public bool fountainState { get; set; } = false;
+
+    public bool MovePlayer(string direction) {
+        if (direction == "north") {
+            if (positionPlayer.x - 1 < 0)
+                return false;
+
+            positionPlayer = (positionPlayer.x - 1, positionPlayer.y);
+        } else if (direction == "west") {
+            if (positionPlayer.y - 1 < 0)
+                return false;
+
+            positionPlayer = (positionPlayer.x, positionPlayer.y - 1);
+        } else if (direction == "east") {
+            if (positionPlayer.y + 1 > sizeGrid.width)
+                return false;
+
+            positionPlayer = (positionPlayer.x, positionPlayer.y + 1);
+        } else if (direction == "west") {
+            if (positionPlayer.y - 1 < 0)
+                return false;
+
+            positionPlayer = (positionPlayer.x, positionPlayer.y - 1);
+        } else if (direction == "south") {
+            if (positionPlayer.x + 1 > sizeGrid.heigth)
+                return false;
+
+            positionPlayer = (positionPlayer.x + 1, positionPlayer.y);
+        }
+
+        return true;
+    }
+
+    public bool CheckWin() {
+        return fountainState == true && positionPlayer == (0, 0);
+    }
+}
---
+++ Level31_FountainOfObjects/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Title = "Fountain of Objects";
+
+Grid grid = new Grid();
+
+while (true) {
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine($"You are in the room at (Row = {grid.positionPlayer.x}, Column={grid.positionPlayer.y})");
+    if (grid.CheckWin()) {
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("The Fountain of Objects has been reactivated, and you have escaped with your life!\nYou win!");
+        break;
+    }
+    if (grid.positionPlayer == (0, 0)) {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("You see light in this room coming from outside the cavern. This is the entrance.");
+    } else if (grid.positionPlayer == grid.positionFountain) {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        if (!grid.fountainState)
+            Console.WriteLine("You hear water dripping in this room. The Fountain of Objects is here!");
+        else
+            Console.WriteLine("You hear the rushing waters from the Fountain of Objects. It has been reactivated!");
+    }
+
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.Write("What do you want to do? ");
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    string? input = Console.ReadLine();
+    string[]? command = input?.Split(" ");
+
+    if (command?[0] == "move") {
+        if (!grid.MovePlayer(command[1])) {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("You can't go there!");
+            continue;
+        }
+    } else if (input == "enable fountain") {
+        if(grid.positionPlayer == grid.positionFountain)
+            grid.fountainState = true;
+    }
+
+}
