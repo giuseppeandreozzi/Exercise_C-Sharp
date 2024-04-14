de3ef505fe3264c0297c334cc7abd8947319a3d7
giuseppeandreozzi <g.andreozzi98@gmail.com>
2024-04-14 13:02:08 +0200

    Added extra functionality to The Fountain Of Objects
    Functionality added: the user can choose the grid size (small, medium or large) and in some room the user can fall in a pit and lose the game

---
+++ Level31_FountainOfObjects/Game.cs
@@ -0,0 +1,105 @@
+﻿public abstract class Grid {
+    public (int width, int heigth) sizeGrid { get; set; }
+    public (int x, int y) positionFountain { get; set; }
+    public (int x, int y)[] pits { get; set; }
+}
+
+public class GridSmall : Grid {
+    public GridSmall() {
+        sizeGrid = (4, 4);
+        positionFountain = (0, 2);
+        pits = [(2, 2)];
+    }
+}
+
+public class GridMedium : Grid {
+    public GridMedium() {
+        sizeGrid = (6, 6);
+        positionFountain = (2, 4);
+        pits = [(2, 2), (4, 3)];
+    }
+}
+
+public class GridLarge : Grid {
+    public GridLarge() {
+        sizeGrid = (8, 8);
+        positionFountain = (4, 6);
+        pits = [(2, 2), (4, 3), (6, 6), (3, 3)];
+    }
+}
+
+public class Game {
+    private Grid grid;
+    public (int x, int y) positionPlayer { get; set; } = (0, 0);
+    public bool fountainState { get; set; } = false;
+
+    public Game() {
+
+    }
+    public Game (Grid grid) {
+        this.grid = grid;
+    }
+
+    public bool MovePlayer(string direction) {
+        if (direction == "north") {
+            if (positionPlayer.x - 1 < 0)
+                return false;
+
+            positionPlayer = (positionPlayer.x - 1, positionPlayer.y);
+        } else if (direction == "west") {
+            if (positionPlayer.y - 1 < 0)
+                return false;
+
+            positionPlayer = (positionPlayer.x, positionPlayer.y - 1);
+        } else if (direction == "east") {
+            if (positionPlayer.y + 1 > grid.sizeGrid.width)
+                return false;
+
+            positionPlayer = (positionPlayer.x, positionPlayer.y + 1);
+        } else if (direction == "west") {
+            if (positionPlayer.y - 1 < 0)
+                return false;
+
+            positionPlayer = (positionPlayer.x, positionPlayer.y - 1);
+        } else if (direction == "south") {
+            if (positionPlayer.x + 1 > grid.sizeGrid.heigth)
+                return false;
+
+            positionPlayer = (positionPlayer.x + 1, positionPlayer.y);
+        }
+
+        return true;
+    }
+
+    public bool CheckWin() {
+        return fountainState == true && positionPlayer == (0, 0);
+    }
+
+    public (int x, int y) GetPositionFountain() {
+        return grid.positionFountain;
+    }
+
+    public bool CheckPits() {
+        foreach ((int row, int column) positionPit in grid.pits)
+        {
+            if (positionPlayer == positionPit)
+                return true;
+        }
+
+        return false;
+    }
+
+    public bool CheckAdjacentRoomsForPits() {
+        (int row, int columns)[] adjacentRooms = [(positionPlayer.x - 1, positionPlayer.y), (positionPlayer.x + 1, positionPlayer.y), (positionPlayer.x, positionPlayer.y - 1), (positionPlayer.x, positionPlayer.y + 1),
+                                                (positionPlayer.x - 1, positionPlayer.y + 1), (positionPlayer.x - 1, positionPlayer.y - 1), (positionPlayer.x + 1, positionPlayer.y + 1), (positionPlayer.x + 1, positionPlayer.y - 1)];
+
+        foreach(var positionPit in grid.pits) {
+            foreach(var positionAdjacentRooms in adjacentRooms) {
+                if (positionPit == positionAdjacentRooms)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+}
--- Level31_FountainOfObjects/Program.cs
+++ Level31_FountainOfObjects/Program.cs
@@ -1,21 +1,49 @@
 ﻿Console.Title = "Fountain of Objects";
 
-Grid grid = new Grid();
+Game game;
 
+while (true) { 
+    Console.Write($"Type which size of game you want to play: small, medium or large: ");
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    string sizeGame = Console.ReadLine();
+
+    if (sizeGame == "small") {
+        game = new Game(new GridSmall());
+    } else if (sizeGame == "medium") {
+        game = new Game(new GridMedium());
+    } else if (sizeGame == "large") {
+        game = new Game(new GridLarge());
+    } else {
+        Console.WriteLine("Incorrect input");
+        continue;
+    }
+
+    break;
+}
 while (true) {
     Console.ForegroundColor = ConsoleColor.White;
-    Console.WriteLine($"You are in the room at (Row = {grid.positionPlayer.x}, Column={grid.positionPlayer.y})");
-    if (grid.CheckWin()) {
+    Console.WriteLine($"You are in the room at (Row = {game.positionPlayer.x}, Column={game.positionPlayer.y})");
+    if (game.CheckPits()) {
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("You fell in a pit. You lose!");
+        break;
+    }
+
+    if (game.CheckWin()) {
         Console.ForegroundColor = ConsoleColor.Magenta;
         Console.WriteLine("The Fountain of Objects has been reactivated, and you have escaped with your life!\nYou win!");
         break;
     }
-    if (grid.positionPlayer == (0, 0)) {
+
+    if (game.CheckAdjacentRoomsForPits())
+        Console.WriteLine("You feel a draft. There is a pit in a nearby room.");
+ 
+    if (game.positionPlayer == (0, 0)) {
         Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine("You see light in this room coming from outside the cavern. This is the entrance.");
-    } else if (grid.positionPlayer == grid.positionFountain) {
+    } else if (game.positionPlayer == game.GetPositionFountain()) {
         Console.ForegroundColor = ConsoleColor.Blue;
-        if (!grid.fountainState)
+        if (!game.fountainState)
             Console.WriteLine("You hear water dripping in this room. The Fountain of Objects is here!");
         else
             Console.WriteLine("You hear the rushing waters from the Fountain of Objects. It has been reactivated!");
@@ -28,14 +56,14 @@ while (true) {
     string[]? command = input?.Split(" ");
 
     if (command?[0] == "move") {
-        if (!grid.MovePlayer(command[1])) {
+        if (!game.MovePlayer(command[1])) {
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("You can't go there!");
             continue;
         }
     } else if (input == "enable fountain") {
-        if(grid.positionPlayer == grid.positionFountain)
-            grid.fountainState = true;
+        if(game.positionPlayer == game.GetPositionFountain())
+            game.fountainState = true;
     }
 
 }
